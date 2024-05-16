f1c8e2e6a4c4add1268175ac0b0ac647cb675c9e
michlaw <m.lawinski@ente.com.pl>
2024-05-16 11:57:57 +0200

    Count used memory for flyweight and without

--- Collections/src/main/java/org/example/Main.java
+++ Collections/src/main/java/org/example/Main.java
@@ -1,29 +1,45 @@
 package org.example;
 
 import org.example.entity.Car;
+import org.example.entity.Item;
 import org.example.entity.Location;
+import org.example.entity.Warehouse;
 import org.example.interfaces.IComponent;
 import org.example.services.ComponentIterator;
+import org.example.services.FlyweightPool;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("START");
-        IComponent s = new Location("Polska");
-        Location l = new Location("Kraków");
-        s.addElement(l);
-        l.addElement(new Car("Fiat"));
-        l.addElement(new Car("Opel"));
-        l = new Location("Warszawa");
-        l.addElement(new Car("Mercedes"));
-        s.addElement(l);
-        s.addElement(new Car("Ford"));
+//        IComponent s = new Location("Polska");
+//        Location l = new Location("Kraków");
+//        s.addElement(l);
+//        l.addElement(new Car("Fiat"));
+//        l.addElement(new Car("Opel"));
+//        l = new Location("Warszawa");
+//        l.addElement(new Car("Mercedes"));
+//        s.addElement(l);
+//        s.addElement(new Car("Ford"));
 //        s.writeInfo();
-        ComponentIterator componentIterator = new ComponentIterator(s);
-        s.writeInfo();
-        while(componentIterator.hasNext()) {
-            componentIterator.getNext().writeInfo();
+//        ComponentIterator componentIterator = new ComponentIterator(s);
+//        s.writeInfo();
+//        while(componentIterator.hasNext()) {
+//            componentIterator.getNext().writeInfo();
+//        }
+        Runtime.getRuntime().gc();
+        Warehouse[] warehouses = new Warehouse[100];
+        for(int i = 0; i < warehouses.length; i++) {
+            warehouses[i] = new Warehouse();
+            for(int j = 0; j < 20; j++)
+                warehouses[i].addItem(FlyweightPool.getItem("art"+j, j));
+//                magazyny[i].addItem(new Item("art"+j,j));
         }
-        System.out.println("STOP");
+        for(Warehouse m: warehouses)
+            for(Item a:m.getItems())
+                System.out.println(a.getName()+" "+a.getPrice());
+
+
+        Long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.println(usedMemory);
     }
 //    Fiat 1
 //    Opel 1
